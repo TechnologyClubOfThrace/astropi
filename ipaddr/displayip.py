54f12b9d942a4db22555687e5163912b3e6d366d
Marios Zikos <marios.zikos@gmail.com>
2019-12-07 11:19:26 +0000

    add LAN Ip to display addr

--- ipaddr/displayip.py
+++ ipaddr/displayip.py
@@ -5,12 +5,15 @@ import fcntl
 import struct
 
 def get_interface_ipaddress(network):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', network[:15])
-    )[20:24])
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', network[:15])
+        )[20:24])
+    except:
+        return ""
 
 
 sense = SenseHat()
@@ -20,7 +23,14 @@ red = (255, 0, 0)
 
 i=0
 while i<2:
-   sense.show_message("My IP:" + get_interface_ipaddress('wlan0'), text_colour=red, scroll_speed=0.06)
+   wlanip = get_interface_ipaddress('wlan0')
+   lanip = get_interface_ipaddress('eth0')
+
+   ipaddr = "Lan:  " + lanip + "  Wifi: " + wlanip
+
+   print (ipaddr)
+
+   sense.show_message(ipaddr, text_colour=red, scroll_speed=0.06)
    i=i+1
 
 
